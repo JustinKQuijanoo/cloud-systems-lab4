3d2ff9a59304159a3c97e502b2f25c908ef7883f
JustinKQuijanoo <quijanju@sheridancollege.ca>
2023-04-15 13:55:57 -0400

    added module for security-group

--- main.tf
+++ main.tf
@@ -13,3 +13,9 @@ module "vpc" {
   sn_green_cidrs     = ["192.168.0.0/24", "192.168.1.0/24", "192.168.2.0/24"]
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
+
+module "security-group" {
+  source    = "./modules/security-group"
+  vpc_id    = module.vpc.vpc_id
+  vpc_names = module.vpc.vpc_names
+}
