d62a0169c0c0d10e717cda7262a0cf78d46dbb66
JustinKQuijanoo <quijanju@sheridancollege.ca>
2023-04-15 21:49:16 -0400

    updated comments

--- modules/high-availability-infrastructure/outputs.tf
+++ modules/high-availability-infrastructure/outputs.tf
@@ -1,3 +1,11 @@
+###
+# Author: Justin Quijano
+# Class: SYST35144 Cloud Systems
+# Lab 4
+###
+
+# output to be used in ./main.tf
+# displays the dns names for load balancer in VPC blue and VPC green
 output "alb_blue_dns_name" {
   description = "DNS name of load balancer for VPC blue"
   value       = aws_lb.LB-JQ-Blue.dns_name
